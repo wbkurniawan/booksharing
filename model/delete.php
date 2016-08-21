bc3072be2c45f1720b3de3672b545633530dccdb
William Kurniawan <wkurniawan@gmail.com>
2016-08-21 20:45:29 +0200

    notification, new book, admin functions

---
+++ model/delete.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: William
+ * Date: 8/7/2016
+ * Time: 10:31 PM
+ */
+
+include_once(__DIR__.'/../model/class/UserSession.php');
+include_once(__DIR__.'/../model/class/Books.php');
+include_once(__DIR__.'/../library/db/Connect.class.php');
+include_once(__DIR__.'/../lock.php');
+header('Content-type: application/json');
+
+
+$bookId = isset($_POST["bookId"])?$_POST["bookId"]:"";
+if(!isset($_SESSION["user"])){
+    $response = array('error' => true,
+                      'error_message' => 'user not logged in',
+                      'error_code' => 403);
+    echo json_encode($response);
+    die();
+}
+
+$userSession =  unserialize($_SESSION["user"]);
+$userId = $userSession->userId;
+
+$book = new Books($bookId);
+
+try{
+    $book->borrowBook($userId);
+    $response = array('error' => false,
+        'error_message' => '');
+}catch(Exception $e) {
+    $response = array('error' => true,
+        'error_message' => $e->getMessage(),
+        'error_code' => 500);
+}
+echo json_encode($response);
