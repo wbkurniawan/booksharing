d3cc6e0f75947b573c280316e2fd15b2f6df0135
Christoph Purnomo <christoph.purnomo@gmail.com>
2016-10-12 00:35:50 +0200

    optimized text image layout

--- about.php
+++ about.php
@@ -17,7 +17,9 @@ include_once(__DIR__.'/header.php');
         <div class="container" id="bookContainer">
             <p>Just like the church has a library, some of the church members surely have a collection of spiritual books on christianity theme. The concept of book sharing is <b>to share and exchange books</b> between readers, especially for those who want to read books relating to the <b>struggles he/she is experiencing</b> and also for those with a deeper longing <b>to learn more</b> about specific themes of the Bible. The church library team hopes to facilitate the readers by means of this book sharing website.</p>
             <p><i>Seperti gereja memiliki sebuah perpustakaan, sebagian orang juga mempunyai koleksi buku-buku rohani. Membaca buku yang berkenaan dengan <b>pergumulan yang dialami</b> saat itu, <b>keingintahuan</b> yang lebih mendalam tentang tema tertentu dalam Alkitab yang berbeda pada setiap orang, maka konsep book sharing diharapkan bisa memperkaya kita dalam hal membaca buku-buku rohani yang telah menjadi berkat bagi orang lain. Komisi perpustakaan memfasilitasi para pembaca buku dengan sarana website book sharing untuk <b>saling berbagi</b> dengan saling meminjamkan buku rohani.</i></p>
-            <img src="assets/img/buechertisch.jpg" alt="buechertisch.jpg">
+            <div class="image-container">
+                <img src="assets/img/buechertisch.jpg" alt="buechertisch.jpg" style="width:100%; height:auto;" />
+            </div>
         </div>
     </div>
     <!--=== End Shop Suvbscribe ===-->
