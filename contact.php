1d2b75f3194a4a6d61c655f042fce243fd19fd74
Christoph Purnomo <christoph.purnomo@gmail.com>
2016-10-12 01:02:14 +0200

    preparing contact.php ... no styling. requirements unclear.

---
+++ contact.php
@@ -0,0 +1,65 @@
+<?php
+$lock = false;
+$aboutMenu = true;
+include_once(__DIR__.'/header.php');
+?>
+    <!--=== Breadcrumbs v4 ===-->
+    <div class="breadcrumbs-v4">
+        <div class="container">
+            <h1>Need help?</h1>
+            <h3>Contact us</h3>
+        </div><!--/end container-->
+    </div>
+    <!--=== End Breadcrumbs v4 ===-->
+
+    <!--=== Content Medium Part ===-->
+    <div class="log-reg-v3 content-md margin-bottom-30">
+         <div class="container">
+            <div class="col-md-7 md-margin-bottom-50">
+                <form id="sky-form4" class="log-reg-block sky-form">
+                    <div class="login-input reg-input">
+                        <section class="register-section">
+                                <textarea class="form-control" rows="3"></textarea>
+                        </section>
+                    </div>
+                    <button class="btn-u btn-u-sea-shop btn-block margin-bottom-20 margin-top-20" type="submit">Send</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!--=== End Shop Suvbscribe ===-->
+
+<?php include_once(__DIR__.'/footer.php'); ?>
+</div><!--/wrapper-->
+
+<!-- JS Global Compulsory -->
+<script src="assets/plugins/jquery/jquery.min.js"></script>
+<script src="assets/plugins/jquery/jquery-migrate.min.js"></script>
+<script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+
+<!-- Get the data -->
+<script src="assets/js/jsrender.js"></script>
+<script src="assets/js/app/header.js"></script>
+
+<!-- JS Implementing Plugins -->
+<script src="assets/plugins/back-to-top.js"></script>
+<script src="assets/plugins/smoothScroll.js"></script>
+<script src="assets/plugins/jquery-steps/build/jquery.steps.js"></script>
+<script src="assets/plugins/scrollbar/js/jquery.mCustomScrollbar.concat.min.js"></script>
+
+<!-- JS Customization -->
+<script src="assets/js/custom.js"></script>
+<!-- JS Page Level -->
+
+<!--[if lt IE 9]>
+    <script src="assets/plugins/respond.js"></script>
+    <script src="assets/plugins/html5shiv.js"></script>
+    <script src="assets/plugins/sky-forms-pro/skyforms/js/sky-forms-ie8.js"></script>
+<![endif]-->
+<!--[if lt IE 10]>
+    <script src="assets/plugins/sky-forms-pro/skyforms/js/jquery.placeholder.min.js"></script>
+<![endif]-->
+
+</body>
+</html>
