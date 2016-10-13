b60de58cc56c91f9dfca310bbfe63eed21536b9e
Christoph Purnomo <christoph.purnomo@gmail.com>
2016-10-13 22:02:52 +0200

    completed FAQ

--- faq.php
+++ faq.php
@@ -14,49 +14,72 @@ include_once(__DIR__.'/header.php');
     <div class="content-md margin-bottom-30" id="eventWrapper">
         <div class="container" id="bookContainer">
             <h2>Questions</h2>
+            <br>
+            <h4>Peminjam</h4>
             <ol class="faq-ol">
                 <li><a href="#1">Bagaimana cara meminjam buku?</a></li>
-                <li><a href="#2">Berapa banyak buku yang bisa dipinjam?</a></li>
-                <li><a href="#3">Apakah ada syarat tertentu untuk dapat meminjam buku?</a></li>
-                <li><a href="#4">Apakah sistem Book Sharing ini gratis?</a></li>
-                <li><a href="#5">Berapa banyak buku yang bisa saya pinjamkan?</a></li>
-                <li><a href="#6">Apa yang harus saya lakukan apabila saya merusak buku yang saya pinjam (sengaja atau tidak)?</a></li>
-                <li><a href="#7">Saya tidak sengaja mengklik permohonan pinjam buku - apakah bisa dibatalkan?</a></li>
+                <li><a href="#2">Apakah arti status pada buku?</a></li>
+                <li><a href="#3">Berapa banyak buku yang bisa dipinjam?</a></li>
+                <li><a href="#4">Apakah ada syarat tertentu untuk dapat meminjam buku?</a></li>
+                <li><a href="#5">Apakah sistem Book Sharing ini gratis?</a></li>
+                <li><a href="#6">Apa yang harus saya lakukan apabila saya merusakkan buku yang dipinjam?</a></li>
+                <li><a href="#7">Saya tidak sengaja mengklik permohonan peminjaman buku - apakah bisa dibatalkan?</a></li>
                 <li><a href="#8">Siapa saja yang boleh meminjam buku?</a></li>
-                <li><a href="#9">Siapa saja yang boleh meminjamkan buku?</a></li>
-                <li><a href="#10">Mengapa buku yang saya daftarkan untuk dipinjamkan belum dapat dipinjam?</a></li>
-                <li><a href="#11">Mengapa yang ada di sini hanya buku-buku rohani Kristen?</a></li>
-                <li><a href="#12">Kemana saya dapat menujukan kritik/apresiasi/saran?</a></li>
+                <li><a href="#9">Mengapa yang ada di sini hanya buku-buku rohani Kristen?</a></li>
+                <li><a href="#10">Kemana saya dapat menujukan kritik/apresiasi/saran?</a></li>
+            </ol>
+            <br>
+            <h4>Pemilik buku</h4>
+            <ol class="faq-ol">
+                <li><a href="#1">Apakah sistem Book Sharing ini gratis?</a></li>
+                <li><a href="#2">Siapa saja yang boleh meminjamkan buku?</a></li>
+                <li><a href="#3">Berapa banyak buku yang bisa saya pinjamkan?</a></li>
+                <li><a href="#4">Apakah buku yang saya pinjamkan harus saya berikan ke admin?</a></li>
+                <li><a href="#5">Mengapa buku yang saya daftarkan untuk dipinjamkan belum dapat dipinjam?</a></li>
+                <li><a href="#6">Apakah buku yang saya pinjamkan akan dijaga dengan baik oleh peminjam?</a></li>
             </ol>
             <br><br>
             <h2>Answers</h2>
+            <br>
+            <h4>Peminjam</h4>
             <ol class="faq-ol">
                 <li><a name="1">Bagaimana cara meminjam buku?</a></li>
                 <div>Untuk meminjam buku anda harus login atau register terlebih dahulu. Pilih buku yang mau dipinjam, lalu klik "Borrow". Setelah itu pemilik buku akan mendapat notifikasi. Bila dia mau meminjamkan pada anda, anda akan mendapat notifikasi dan kemudian bisa membuat janji untuk mengambil buku tersebut. Tetapi apabila ada suatu alasan yang membuat buku tersebut tidak dapat dipinjamkan, dia akan memberikan alasannya pada anda.</div>
-                <li><a name="2">Berapa banyak buku yang bisa dipinjam?</a></li>
-                <div>Saat ini setiap orang hanya dapat meminjam satu buku. Ketika jumlah buku yang terdaftar sudah lebih banyak, maka admin akan mempertimbangkan untuk mengijinkan peminjaman lebih dari satu buku.</div>
-                <li><a name="3">Apakah ada syarat tertentu untuk dapat meminjam buku?</a></li>
-                <div>Tidak. Tetapi anda wajib menjaga keadaan buku yang dipinjam tetap dalam keadaan baik dan dikembalikan tepat waktu.</div>
-                <li><a name="4">Apakah sistem Book Sharing ini gratis?</a></li>
+                <li><a name="2">Apakah arti status pada buku?</a></li>
+                <div>Available - dapat dipinjam. Reserved - buku sudah direservasi namun belum diberikan ke peminjam. Pending Admin Approval - buku sedang menunggu persetujuan Admin untuk dapat dipinjamkan. Borrowed - buku sedang dipinjam. Private - buku sedang digunakan oleh pemilik buku sendiri atau sedang dipinjamkan di luar sistem Book Sharing.</div>
+                <li><a name="3">Berapa banyak buku yang bisa dipinjam?</a></li>
+                <div>Saat ini setiap orang hanya dapat meminjam 1 buku. Ketika jumlah buku yang terdaftar sudah lebih banyak, maka admin akan mempertimbangkan untuk mengijinkan peminjaman lebih dari satu buku.</div>
+                <li><a name="4">Apakah ada syarat tertentu untuk dapat meminjam buku?</a></li>
+                <div>Tidak. Tetapi anda wajib menjaga keadaan buku yang dipinjam tetap dalam keadaan baik (halaman buku TIDAK boleh dilipat atau ditandai) dan dikembalikan pada waktunya.</div>
+                <li><a name="5">Apakah sistem Book Sharing ini gratis?</a></li>
                 <div>Ya. Anda sebagai pemilik dan peminjam buku tidak perlu membayar apa-apa.</div>
-                <li><a name="5">Berapa banyak buku yang bisa saya pinjamkan?</a></li>
-                <div>Sebanyak yang anda kehendaki/relakan.</div>
-                <li><a name="6">Apa yang harus saya lakukan apabila saya merusak buku yang saya pinjam (sengaja atau tidak)?</a></li>
-                <div>Beritahukan pada admin dan pemilik buku. Tanyakan pada pemilik buku apa yang dia kehendaki sebagai bentuk kompensasi.</div>
-                <li><a name="7">Saya tidak sengaja mengklik permohonan pinjam buku - apakah bisa dibatalkan?</a></li>
+                <li><a name="6">Apa yang harus saya lakukan apabila saya merusak buku yang saya pinjam?</a></li>
+                <div>Beritahukan tentang kerusakkan pada admin dan pemilik buku. Tanyakan pada pemilik buku apa yang dia kehendaki sebagai bentuk kompensasi.</div>
+                <li><a name="7">Saya tidak sengaja mengklik permohonan peminjaman buku - apakah bisa dibatalkan?</a></li>
                 <div>Bisa. Di halaman setelah anda menekan tombol pinjam (borrow), anda dapat membatalkan permohonan peminjaman (cancel). Apabila anda sudah terlanjur menutup halaman tersebut, anda juga dapat membatalkan dari halaman My Books.</div>
                 <li><a name="8">Siapa saja yang boleh meminjam buku?</a></li>
-                <div></div>
-                <li><a name="9">Siapa saja yang boleh meminjamkan buku?</a></li>
-                <div></div>
-                <li><a name="10">Mengapa buku yang saya daftarkan untuk dipinjamkan belum dapat dipinjam?</a></li>
-                <div>Karena admin perlu memeriksa dahulu kelayakan buku tersebut untuk dapat dimasukkan ke dalam sistem.</div>
-                <li><a name="11">Mengapa yang ada di sini hanya buku-buku rohani Kristen?</a></li>
-                <div>Karena platform ini bernaung dibawah gereja Freie evangelische Gemeinde Immanuel Berlin. Platform ini dibuat dengan tujuan untuk memenuhi kebutuhan rohani jemaat.</div>
-                <li><a name="12">Kemana saya dapat menujukan kritik/apresiasi/saran?</a></li>
-                <div></div>
+                <div>Setiap orang yang sudah mendaftarkan diri di Book Sharing ini.</div>
+                <li><a name="9">Mengapa yang ada di sini hanya buku-buku rohani Kristen?</a></li>
+                <div>Karena platform ini bernaung dibawah gereja Freie evangelische Gemeinde Immanuel Berlin dan dibuat dengan tujuan untuk memenuhi kebutuhan rohani jemaat.</div>
+                <li><a name="10">Kemana saya dapat menujukan pertanyaan/kritik/apresiasi/saran?</a></li>
+                <div>Silakan layangkan email ke <a href="mailto:komisiperpustakaan@immanuel-berlin.de">komisiperpustakaan@immanuel-berlin.de</a>.</div>
             </ol>
-            <br><br>
+            <br>
+            <h4>Pemilik buku</h4>
+            <ol class="faq-ol">
+                <li><a name="1">Apakah sistem Book Sharing ini gratis?</a></li>
+                <div>Ya. Anda sebagai pemilik dan peminjam buku tidak perlu membayar apa-apa.</div>
+                <li><a name="2">Siapa saja yang boleh meminjamkan buku?</a></li>
+                <div>Setiap orang yang telah mendaftarkan diri di Book Sharing ini.</div>
+                <li><a name="3">Berapa banyak buku yang bisa saya pinjamkan?</a></li>
+                <div>Sebanyak yang anda kehendaki/relakan.</div>
+                <li><a name="4">Apakah buku yang saya pinjamkan harus saya berikan ke admin?</a></li>
+                <div>Tidak. Buku tersebut tetap berada di pemilik buku. Pemilik buku akan diberi tahu kalau ada yg berminat meminjam buku tersebut.</div>
+                <li><a name="5">Mengapa buku yang saya daftarkan untuk dipinjamkan belum dapat dipinjam?</a></li>
+                <div>Karena admin perlu memeriksa dahulu kelayakan buku tersebut untuk dapat dimasukkan ke dalam sistem.</div>
+                <li><a name="6">Apakah buku yang saya pinjamkan akan dijaga dengan baik oleh peminjam?</a></li>
+                <div>Peminjam wajib menjaga keadaan buku agar tetap baik. Jika buku tersebut rusak (sengaja atau tidak), pemilik berhak menentukan kompensasi yang pantas dan admin akan membantu menyelesaikan prosesnya.</div>
+            </ol>
             <div style="margin-bottom:50px" class="row">
 
             </div>
