ec83600f37aae68d013d48366bcc3fa613c4cff3
ssunarja <stefanus.sunarja@gmail.com>
2016-10-15 21:00:04 +0200

    Fix image width and text boldness

--- about.php
+++ about.php
@@ -7,7 +7,7 @@ include_once(__DIR__.'/header.php');
     <div class="breadcrumbs-v4">
         <div class="container">
             <h1>About</h1>
-            <h3>What is BookSharing?</h3>
+            <h3>What is BookSharing and Why?</h3>
         </div><!--/end container-->
     </div>
     <!--=== End Breadcrumbs v4 ===-->
@@ -15,11 +15,9 @@ include_once(__DIR__.'/header.php');
     <!--=== Content Medium Part ===-->
     <div class="content-md margin-bottom-30" id="eventWrapper">
         <div class="container" id="bookContainer">
-            <p>Just like the church has a library, some of the church members surely have a collection of spiritual books on christianity theme. The concept of book sharing is <b>to share and exchange books</b> between readers, especially for those who want to read books relating to the <b>struggles he/she is experiencing</b> and also for those with a deeper longing <b>to learn more</b> about specific themes of the Bible. The church library team hopes to facilitate the readers by means of this book sharing website.</p>
-            <p><i>Seperti gereja memiliki sebuah perpustakaan, sebagian orang juga mempunyai koleksi buku-buku rohani. Membaca buku yang berkenaan dengan <b>pergumulan yang dialami</b> saat itu, <b>keingintahuan</b> yang lebih mendalam tentang tema tertentu dalam Alkitab yang berbeda pada setiap orang, maka konsep book sharing diharapkan bisa memperkaya kita dalam hal membaca buku-buku rohani yang telah menjadi berkat bagi orang lain. Komisi perpustakaan memfasilitasi para pembaca buku dengan sarana website book sharing untuk <b>saling berbagi</b> dengan saling meminjamkan buku rohani.</i></p>
-            <div class="image-container">
-                <img src="assets/img/buechertisch.jpg" alt="buechertisch.jpg" style="width:100%; height:auto;" />
-            </div>
+            <p>Just like the church has a library, some of the church members surely have a collection of spiritual books on christianity theme. <strong>The concept of book sharing is to share and exchange books between readers, especially for those who want to read books relating to the struggles he/she is experiencing and also for those with a deeper longing to learn more about specific themes of the Bible.</strong> The church library team hopes to facilitate the readers by means of this book sharing website.</p>
+            <p><i>Seperti gereja memiliki sebuah perpustakaan, sebagian orang juga mempunyai koleksi buku-buku rohani. <strong>Membaca buku yang berkenaan dengan pergumulan yang dialami saat itu, keingintahuan yang lebih mendalam tentang tema tertentu dalam Alkitab yang berbeda pada setiap orang, maka konsep book sharing diharapkan bisa memperkaya kita dalam hal membaca buku-buku rohani yang telah menjadi berkat bagi orang lain.</strong> Komisi perpustakaan memfasilitasi para pembaca buku dengan sarana website book sharing untuk saling berbagi dengan saling meminjamkan buku rohani.</i></p>
+                <img class="buechertisch" src="assets/img/buechertisch.jpg" alt="buechertisch.jpg"/>
         </div>
     </div>
     <!--=== End Shop Suvbscribe ===-->
